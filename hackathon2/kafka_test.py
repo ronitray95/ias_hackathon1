6a52d09fe1b83ed770514f90a5989c07205d3009
Ronit <ronitray95@gmail.com>
2021-04-03 19:03:28 +0530

    Add kafka module. Details in readme

---
+++ hackathon2/kafka_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from kafka import KafkaConsumer
+from json import loads
+
+from models import *
+
+consumer = KafkaConsumer(KAFKA_TOPIC_SERVER_LIST, bootstrap_servers=[
+                         'localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group', value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    message = message.value
+    print(message)
